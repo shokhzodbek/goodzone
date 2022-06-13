8165c20a4217cadfa87b96f9a3fa65c28d0b7d9b
shokhzod <samariddinovshokhzod@gmail.com>
2022-06-13 07:56:01 +0500

    new

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,47 +1,61 @@
-import React from 'react';
-import Carusel1 from '../components/fragments/Carusel';
-import CardItem from '../components/fragments/CardItem';
+import React from "react";
+import Carusel1 from "../components/fragments/Carusel";
+import CardItem from "../components/fragments/CardItem";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Banner from '../components/fragments/Banner';
-import './HomePage.css';
-import BannerOn from '../components/fragments/BannerOn';
-import News from '../components/fragments/News';
-import Brands from '../components/fragments/Brands';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Banner from "../components/fragments/Banner";
+import "./HomePage.css";
+import BannerOn from "../components/fragments/BannerOn";
+import News from "../components/fragments/News";
+import Brands from "../components/fragments/Brands";
 
 function HomePage() {
-	return (
-		<div className="home-page">
-			{/* carusel */}
-			<Carusel1 />
-			{/* items */}
-			<div className="home-item">
-				<p>Yange mahsulotlar</p>
-				<CardItem />
-			</div>
-			{/* banner */}
-			<Banner />
-			{/* items */}
-			<div className="home-item">
-				<p>Yange mahsulotlar</p>
-				<CardItem />
-			</div>
-			{/* banner */}
-			<BannerOn />
-			{/* items */}
-			<div className="home-item">
-				<p>Yange mahsulotlar</p>
-				<CardItem />
-			</div>
-			<Brands />
-			<News />
+  return (
+    <div className="home-page">
+      {/* carusel */}
+      <Carusel1 />
+      {/* items */}
+      <div className="home-item">
+        <p>Yange mahsulotlar</p>
+        <CardItem />
+      </div>
+      <div className="home-item">
+        <CardItem />
+      </div>
+      <div className="home-item">
+        <CardItem />
+      </div>
+      {/* banner */}
+      <Banner />
+      {/* items */}
+      <div className="home-item">
+        <p>Mahsulotlar</p>
+        <CardItem />
+      </div>
+      {/* banner */}
+      <BannerOn />
+      {/* items */}
+      <div className="home-item">
+        <p>Yange mahsulotlar</p>
+        <CardItem />
+      </div>
+      <div className="home-item">
+        <p>Yange mahsulotlar</p>
+        <CardItem />
+      </div>
+      <div className="home-item">
+        <p>Yange mahsulotlar</p>
+        <CardItem />
+      </div>
+      <Brands />
+      <News />
 
-			{/* items */}
-			{/* banner */}
-			{/* company logo */}
-			{/* news */}
-		</div>
-	);
+      {/* items */}
+      {/* banner */}
+      {/* company logo */}
+      {/* news */}
+    </div>
+  );
 }
 
 export default HomePage;
